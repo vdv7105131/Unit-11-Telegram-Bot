d792ccfea031f8cb751f7212b6555b0438db43a0
vdv7105131 <vdv7105131@gmail.com>
2021-12-01 14:13:36 +0300

    ok

--- Unit 11 Telegram Bot/Program.cs
+++ Unit 11 Telegram Bot/Program.cs
@@ -6,51 +6,21 @@ namespace Unit_11_Telegram_Bot
 {
     class Program
     {
-        static ITelegramBotClient botClient;
         static void Main(string[] args)
         {
-            
-            // Теперь, используя токен, создадим клиент для работы с ботом:
-            /// <summary>
-            ///Это объект класса, который позволяет взаимодействовать с конкретным ботом. Этот объект обрабатывает все обращения к боту. 
-            /// </summary>
-            var botClient = new TelegramBotClient(BotCredentials.BotToken);
+            var bot = new BotWorker();
+            bot.Inizalize();
+            bot.Start();
 
-            /// <summary>
-            ///Это базовый метод, он возвращает описание бота
-            /// </summary>
-            botClient.GetMeAsync();
+            Console.WriteLine("Напишите stop для прекращения работы");
 
-            // проверка работоспособности
-            var me = botClient.GetMeAsync().Result;
-            Console.WriteLine("Всем привет! Меня зовут {0}.", me.FirstName);
-
-
-            // плохое решение
-            botClient = new TelegramBotClient(BotCredentials.BotToken);
-
-            botClient.OnMessage += Bot_OnMessage;
-            botClient.StartReceiving(); // старт получения сообщений
-
-            Console.WriteLine("Нажмите любую кнопку для остановки");
-            Console.ReadKey();
-
-            botClient.StopReceiving(); // окончанчание получения сообщений
-
-        }
-        /// <summary>
-        /// метод, который выполняется, когда к нам приходит сообщение
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        static async void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Text != null)
+            string command;
+            do
             {
-                Console.WriteLine($"Получено сообщение в чате: {e.Message.Chat.Id}.");
-
-                await botClient.SendTextMessageAsync(
-                chatId: e.Message.Chat, text: "Вы написали:\n" + e.Message.Text);
+                command = Console.ReadLine();
+            } while (command != "stop");
+            {
+                bot.Stop();
             }
         }
     }
@@ -63,6 +33,63 @@ namespace Unit_11_Telegram_Bot
         /// </summary>
         public static readonly string BotToken = "2136604647:AAH1ghd5fbxOhdW_-6ZU-fFgAIEOgXHUv-4";
 
+    }
 
+    public class BotWorker
+    {
+        private ITelegramBotClient botClient;
+
+        /// <summary>
+        /// создаем клиент бота
+        /// </summary>
+        public void Inizalize()
+        {
+            botClient = new TelegramBotClient(BotCredentials.BotToken);
+        }
+
+        /// <summary>
+        /// старт получения сообщений
+        /// </summary>
+        public void Start()
+        {
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+        }
+
+        /// <summary>
+        /// окончанчание получения сообщений
+        /// </summary>
+        public void Stop()
+        {
+            botClient.StopReceiving();
+        }
+
+        /// <summary>
+        /// метод, который выполняется, когда к нам приходит сообщение
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Получено сообщение в чате: {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                chatId: e.Message.Chat, text: "Вы написали сообщение:\n" + e.Message.Text);
+            }
+        }
+    }
+
+    class BotMessageLogic
+    {
+        private BotMessageLogic logic;
+        private ITelegramBotClient botClient;
+
+        public void Inizalize()
+        {
+            botClient = new TelegramBotClient(BotCredentials.BotToken);
+            logic = new BotMessageLogic();
+        }
     }
 }
